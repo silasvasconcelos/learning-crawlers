ae04c88e65804033e93042e75a17945224b5955c
Silas <silasvasconcelos@hotmail.com.br>
2019-06-09 01:59:49 -0300

    created crawler to posts of the blog of scrapinghub

---
+++ crawler/spiders/blog_scrapinghub.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class BlogScrapinghubSpider(scrapy.Spider):
+    name = 'blog_scrapinghub'
+    allowed_domains = ['blog.scrapinghub.com']
+    start_urls = ['https://blog.scrapinghub.com/']
+
+    def parse(self, response):
+        posts = response.css(
+            'div.post-listing a.more-link::attr(href)').getall()
+        for post in posts:
+            yield response.follow(post, self.parse_post)
+
+        next_page = response.css('a.next-posts-link::attr(href)').get()
+        if next_page:
+            yield response.follow(next_page, self.parse)
+
+    def parse_post(self, response):
+        yield {
+            'title': response.css('div.post-page-content div.post-header h1 span::text').get(),
+            'posted_at': response.css('div.post-page-content div.post-header div.byline span a::text').get(),
+            'posted_by_name': response.css('div.post-page-content div.post-header div.byline span.author a::text').get(),
+            'posted_by_prifile_url': response.css('div.post-page-content div.post-header div.byline span.author a::attr(href)').get(),
+            'body': response.css('div.post-page-content div.post-body span').get(),
+        }
