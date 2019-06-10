12b723efd4bce11f16e5b62912d687e4ff90de33
Silas <silasvasconcelos@hotmail.com.br>
2019-06-10 01:48:02 -0300

    added pipelines to storage in the mongodb and validate unique post

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -5,7 +5,42 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from scrapy.exceptions import DropItem
+from pymongo import MongoClient
 
-class CrawlerPipeline(object):
+class MongodbBase(object):
+    mongo_uri = 'mongodb://localhost:27017'
+    mongo_db = 'crawler_blog_scrapinghub'
+    mongo_collection_name = 'posts'
+
+    def get_collection(self, collection=None):
+        return None if collection is None else self.db[collection]
+
+    def open_spider(self, spider):
+        self.client = MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+    
+    def close_spider(self, spider):
+        self.client.close()
+
+class UniquePostPipeline(MongodbBase):
+
+    def open_spider(self, spider):
+        super(UniquePostPipeline, self).open_spider(spider)
+        posts = self.get_collection(self.mongo_collection_name).find({}, {'url': True})
+        urls = [ p.get('url') for p in posts ]
+        self.__post_storageds = urls
+
     def process_item(self, item, spider):
+        item_url = item.get('url')
+        if item_url in self.__post_storageds:
+            raise DropItem("Duplcate item found: %s" % item.get('title', 'Unknown'))
+        else:
+            self.__post_storageds.append(item_url)
+            return item
+
+class MongodbStoragePostPipeline(MongodbBase):
+    
+    def process_item(self, item, spider):
+        self.get_collection(self.mongo_collection_name).insert_one(item)
         return item
