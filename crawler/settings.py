a030d896489556b2263cdc39480d1cf1ea5c0567
Silas <silasvasconcelos@hotmail.com.br>
2019-06-10 01:50:13 -0300

    added pipelines to settings

--- crawler/settings.py
+++ crawler/settings.py
@@ -64,9 +64,10 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'crawler.pipelines.CrawlerPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'crawler.pipelines.UniquePostPipeline': 800,
+   'crawler.pipelines.MongodbStoragePostPipeline': 900,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
